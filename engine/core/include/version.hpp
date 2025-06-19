50d0f689ab6d9452f90cd79a13d2e393afb1eed1
Heejun Song <pasteldew@gmail.com>
2025-06-19 22:11:23 +0900

    Fix invalid codes for CI

--- engine/core/include/version.hpp
+++ engine/core/include/version.hpp
@@ -1,27 +1,25 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
-#include <tuple>
 
-namespace lp_engine
-{
-namespace version
-{
-    bool isVersionAtLeast(uint32_t major, uint32_t minor, uint32_t patch);
-    bool isVersionLessThan(uint32_t major, uint32_t minor, uint32_t patch);
-    bool isVersionEqualTo(uint32_t major, uint32_t minor, uint32_t patch);
-    bool isVersionEqualTo(const std::string& version);
-    
-    uint32_t getMajor();
-    uint32_t getMinor();
-    uint32_t getPatch();
-    std::string getPrerelease();
-    std::string getProjectVersion();
-    std::string getProjectFullVersion();
-    std::string getGitCommitHash();
-    
-    std::tuple<uint32_t, uint32_t, uint32_t> getSemanticVersion();
-    
-    bool isPrerelease();
-}
-}
+namespace lp_engine {
+namespace version {
+bool isVersionAtLeast(uint32_t major, uint32_t minor, uint32_t patch);
+bool isVersionLessThan(uint32_t major, uint32_t minor, uint32_t patch);
+bool isVersionEqualTo(uint32_t major, uint32_t minor, uint32_t patch);
+bool isVersionEqualTo(const std::string &version);
+
+uint32_t getMajor();
+uint32_t getMinor();
+uint32_t getPatch();
+std::string getPrerelease();
+std::string getProjectVersion();
+std::string getProjectFullVersion();
+std::string getGitCommitHash();
+
+std::tuple<uint32_t, uint32_t, uint32_t> getSemanticVersion();
+
+bool isPrerelease();
+} // namespace version
+} // namespace lp_engine
