ad954d97da2b8bd9c2c8604be5338e16c0cef5a1
Heejun Song <pasteldew@gmail.com>
2025-07-29 23:21:36 +0900

    러스트로 급선회

---
+++ legacy/engine/core/include/core.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+namespace lpe
+{
+    namespace core
+    {
+        // Core classes and functionalities will be declared here
+        // This is a placeholder for the actual implementation
+        class CoreEngine
+        {
+        public:
+            CoreEngine();
+            ~CoreEngine();
+
+            void initialize();
+            void run();
+            void shutdown();
+
+        private:
+            // Private members and methods for the core engine functionality
+        };
+    } // namespace core
+}
+    // Forward declarations of core classes
+    // class Application;
+    // class Window;
+    // class Renderer;
+    // class InputManager;
+    // class SceneManager;
+    // class ResourceManager;
+    // class EventManager;
+    // class PhysicsManager;
+    // class AudioManager;
+    // class UIManager;
+    // class TimeManager;
+    // class DebugManager;
+    // class SettingsManager;
+    // class TaskManager;
+    // class NetworkManager;
+    // class ScriptManager;
+    // class LocalizationManager;
+    // class FileSystemManager;
+    // class PerformanceManager;
+    // class PluginManager;
+    // class TaskScheduler;
+    // class AssetManager;
+    // class GraphicsPipeline;
+    // class ShaderManager;
+    // class MaterialManager;
+    // class TextureManager;
+    // class AnimationManager;
+    // class InputSystem;
+    // class EntityManager;
+    // class ComponentManager;
+    // class SceneGraph;
+    // class CameraManager;
+    // class LightManager;
+    // class ParticleSystemManager;
+    // class RenderPassManager;
+    // class PostProcessingManager;
